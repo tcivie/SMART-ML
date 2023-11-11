59c37ccb1ec2a46913105da476ac2d5007b3119c
Gleb Tcivie <gleb@tcivie.com>
2023-11-11 22:39:16 +0200

    Created sumo server which would handle multiple simulations and would allow to get states and send states to the sim

--- dockerImage/init.py
+++ dockerImage/init.py
@@ -1,45 +1,111 @@
 import os
+from typing import Union, Tuple, Any, List
+
 import traci
 from pathlib import Path
+from flask import Flask, request, jsonify
+import threading
+import uuid
+import socket
 
-# Retrieve the SUMO configuration file name from the environment variable
-sumo_conf_file = os.getenv('SUMO_CONF_FILE', 'run.sumocfg')
+app = Flask(__name__)
 
-# Construct the full path to the configuration file in /sumo-data directory
-config_path = Path('/sumo-data') / sumo_conf_file
+# Dictionary to keep track of simulations
+simulations = {}
 
-# The command to start SUMO, using the full path to the configuration file
-sumoCmd = ["sumo", "-c", str(config_path)]
 
-# Start SUMO with TraCI
-print("Starting SUMO with TraCI...")
-traci.start(sumoCmd)
-print("SUMO started.")
+def start_sumo(config_path: str, port: int) -> None:
+    sumoCmd = ['sumo', '-c', config_path, '--remote-port', str(port)]
+    traci.start(sumoCmd)
 
-# Run the simulation for a certain number of steps
-for step in range(1000):  # Adjust the number of steps as needed
-    traci.simulationStep()
 
-    # Display the current simulation time
-    print(f"Simulation time: {traci.simulation.getTime()}")
+def find_available_port() -> int:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
 
-    # Print the IDs of all vehicles in the simulation
-    vehicles = traci.vehicle.getIDList()
-    print(f"Vehicles on the road: {vehicles}")
 
-    # Get and print the speed of each vehicle
-    for veh_id in vehicles:
-        speed = traci.vehicle.getSpeed(veh_id)
-        print(f"Vehicle {veh_id} speed: {speed} m/s")
+@app.route('/start', methods=['GET'])
+def start_simulation():
+    config_path = request.args.get('config')
+    if not config_path:
+        return "No config path provided", 400
 
-    # Example: Manipulating traffic lights (if applicable)
-    # Get the IDs of all traffic lights
-    traffic_lights = traci.trafficlight.getIDList()
-    for tl_id in traffic_lights:
-        # You can change the state of traffic lights here
-        # traci.trafficlight.setRedYellowGreenState(tl_id, "GreenState")
-        pass
+    session_id = str(uuid.uuid4())
+    port = find_available_port()
 
-# Close the TraCI connection
-traci.close()
-print("TraCI connection closed.")
+    thread = threading.Thread(target=start_sumo, args=(config_path, port))
+    thread.start()
+
+    simulations[session_id] = {
+        'thread': thread,
+        'port': port
+    }
+
+    return jsonify({
+        'sessionId': session_id,
+    })
+
+
+@app.route('/traffic_lights', methods=['GET'])
+def get_traffic_lights(session_id: str) -> Union[tuple[str, int], tuple[List, int]]:
+    if not session_id:
+        return "No session ID provided", 400
+
+    port = simulations[session_id]['port']
+    traci.init(port=port)
+    traffic_light_ids = traci.trafficlight.getIDList()
+    traci.close()
+    return traffic_light_ids, 200
+
+
+@app.route('/set_traffic_light_phase', methods=['POST'])
+def set_traffic_light_phase():
+    session_id = request.args.get('sessionId')
+    if not session_id:
+        return "No session ID provided", 400
+
+    port = simulations[session_id]['port']
+    traci.init(port=port)
+
+    traffic_light_id = request.args.get('trafficLightId')
+    if not traffic_light_id:
+        return "No traffic light ID provided", 400
+
+    phase = request.args.get('phase')
+    if not phase:
+        return "No phase provided", 400
+
+    traci.trafficlight.setPhase(traffic_light_id, int(phase))
+    traci.close()
+
+    return jsonify({
+        'status': 'success'
+    })
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
+# # Retrieve the SUMO configuration file name from the environment variable
+# sumo_conf_file = os.getenv('SUMO_CONF_FILE', 'run.sumocfg')
+#
+# # Construct the full path to the configuration file in /sumo-data directory
+# config_path = Path('/sumo-data') / sumo_conf_file
+#
+# # The command to start SUMO, using the full path to the configuration file
+# sumoCmd = ["sumo", "-c", str(config_path)]
+#
+# # Start SUMO with TraCI
+# print("Starting SUMO with TraCI...")
+# traci.start(sumoCmd, port=8080)
+# print("SUMO started.")
+#
+# while traci.simulation.getMinExpectedNumber() > 0:
+#     traci.simulationStep()
+#
+# # Close the TraCI connection
+# traci.close()
+# print("TraCI connection closed.")
