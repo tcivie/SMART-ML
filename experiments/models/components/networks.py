b54fc02af8c0fc4bff369b00abb8ffadcf1fc8f6
tcivie <gleb@tcivie.com>
2024-06-10 11:10:58 +0300

    Added another run with multiple variations of LSTM network

--- experiments/models/components/networks.py
+++ experiments/models/components/networks.py
@@ -118,6 +118,8 @@ class LSTMNetwork(nn.Module):
         super(LSTMNetwork, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
+        self.state_size = input_size
+        self.action_size = output_size
 
     def forward(self, x):
         h0 = torch.zeros(self.lstm.num_layers, x.size(0), self.lstm.hidden_size).to(x.device)
